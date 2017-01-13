3a9d62f502a5d6a585751b16f2553026f19b0f36
to-4 <ogawa.gmai@gmail.com>
2017-01-13 12:48:38 +0900

    メモ概要ファイル(index.txt) を作成等。

---
+++ test170111.el
@@ -0,0 +1,11 @@
+;; 
+;; [2017.01.11]
+;;  ミニバッファに"Hello, world!" と表示したい
+;; 
+;; [補足]
+;; - コメントアウトは、"M-;"/"M-x comment-region"
+;; - "Emacs-Lisp"へのモード変更は、"M-x emacs-lisp-mode"
+;; - 実行は、()の後ろで "C-x C-e"
+;; 
+(message "Hello, world")
+
