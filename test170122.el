1ec33fea5e712e567486a35cd72b77edbf673b3d
to-4 <ogawa.gmai@gmail.com>
2017-02-07 12:17:19 +0900

    modify test170122.el

--- test170122.el
+++ test170122.el
@@ -2,20 +2,16 @@
 ;; [2017.01.22] 
 ;; loop 文の書き方
 ;; 
-;; - loop マクロを使用
-;; - 
-;;
-
-
-;; loop for VAR from START to END の書き方
-;; (loop for [変数] from [開始数値] to [終了数値]
-;;   (条件がfalse(nil)以外の時のフォームを記述)
-;;   (条件がfalse(nil)   の時のフォームを複数記述)
-;; )
+;; 書式: (dolist ([変数名] [リスト])
+;;         .. [内容])
 ;; 
+;;
+;;
 (setq sum 0)
-(loop for i from 1 to 10
-      (+ sum 1))
+(dolist (n '(1 2 3 4))
+  (setq sum (+ n sum))
+  (princ n)
+  )
 (message "sum: %d" sum)
 
 
