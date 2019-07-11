e79a104fc5141eb1673c14ff49e2320156a913b0
to-4 <ogawa.gmai@gmail.com>
2019-07-11 12:31:39 +0900

    [2019.07.10] Helm インストール方法　※コメント修正

--- init190710.el
+++ init190710.el
@@ -5,8 +5,14 @@
 ;;  - "helm.el"の拡張機能で、実現する
 ;;    拡張機能は、
 ;;      まずに、"M-x package-refresh-contents"
-;;      次にに、"M-x package-list-packages" から、"helm" を選んでインストール
-ル
+;;      次に、"M-x package-list-packages" から、"helm" を選んでインストール
+;;
+;;    2019.7.11
+;;      上記の方法では、"M-x helm-for-files" を実行すると、"Invalid function B" と表示されてしまった
+;;      下記の方法でインストールしたところ、以上のエラーはなくなった。
+;;        M-x package-install<RET>
+;;        helm
+;;
 ;;  - その後は、下記の様に、"init.el"に追加
 ;; --------------------------------------------------------
 
